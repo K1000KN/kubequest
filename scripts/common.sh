346b641df606092c804ac1ae9cabc2333f2c5009
K1000KN <60380299+K1000KN@users.noreply.github.com>
2024-04-04 10:04:03 +0200

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -57,13 +57,28 @@ sudo sysctl --system
 # sudo systemctl daemon-reload
 # sudo systemctl enable crio --now
 
-sudo apt-get update && sudo apt-get install -y containerd.io
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y containerd.io
 sudo mkdir -p /etc/containerd
 sudo containerd config default | sudo tee /etc/containerd/config.toml
 sudo systemctl restart containerd
 sudo systemctl enable containerd --now
 
-echo "CRI runtime installed susccessfully"
 
 # Install kubelet, kubectl and Kubeadm
 
