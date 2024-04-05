a339094e55ed1f6c4be23cac4e9acd4f18ba3758
K1000KN <k1000kn@protonmail.com>
2024-04-06 01:49:21 +0200

    fix

--- scripts/common.sh
+++ scripts/common.sh
@@ -33,9 +33,11 @@ echo \
 "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
 
+sudo apt-get update -y
+
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-    sudo MKdir /etc/docker
+    sudo mkdir /etc/docker
     cat <<EOF | sudo tee /etc/docker/daemon.json
     {
       "exec-opts": ["native.cgroupdriver=systemd"],
