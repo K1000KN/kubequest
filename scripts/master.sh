0f0b0481b35bba6a5aae70a651e66991fc63ff30
K1000KN <k1000kn@protonmail.com>
2024-04-06 21:08:56 +0200

    fix master.sh and common.sh

--- scripts/master.sh
+++ scripts/master.sh
@@ -2,14 +2,21 @@
 #
 # Setup for Control Plane (Master) servers
 
+set -euxo pipefail
+
+NODENAME=$(hostname -s)
+POD_CIDR="192.168.0.0/16"
+
 # Pull required images
 
 sudo kubeadm config images pull
 
 # Initialize kubeadm based on PUBLIC_IP_ACCESS
-
 MASTER_PUBLIC_IP=$(curl ifconfig.me && echo "")
-sudo kubeadm init --apiserver-advertise-address="$MASTER_PUBLIC_IP"
+sudo kubeadm init --control-plane-endpoint="$MASTER_PUBLIC_IP" --apiserver-cert-extra-sans="$MASTER_PUBLIC_IP" --pod-network-cidr="$POD_CIDR" --node-name "$NODENAME" --ignore-preflight-errors Swap
 
-# Deploy Weave network
-kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+# Configure kubeconfig
+
+mkdir -p "$HOME"/.kube
+sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
+sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
